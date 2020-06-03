84da2f54a9d9616b66ad3dec07d3797549a57317
KieranWardle <kieranwardle95@gmail.com>
2020-06-03 18:01:36 +0100

    Added GSuite queue config

--- src/main/java/uk/gov/ons/fsdr/common/ActionQueueConfig.java
+++ src/main/java/uk/gov/ons/fsdr/common/ActionQueueConfig.java
@@ -5,4 +5,6 @@ public class ActionQueueConfig {
   public static final String ACTION_RESULT_ROUTING_KEY = "action.result.request";
   public static final String ACTION_RESULT_EXCHANGE = "action.result.exchange";
 
+  public static final String GSUITE_ACTIONS_ROUTING_KEY = "gsuite.actions.request";
+
 }
