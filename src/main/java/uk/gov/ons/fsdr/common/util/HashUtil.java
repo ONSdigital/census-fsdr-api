b477e1fc103eabf94b5401b1d22d470ef5e23ffa
KieranWardle <kieranwardle95@gmail.com>
2020-06-09 16:48:16 +0100

    Made it tidier

--- src/main/java/uk/gov/ons/fsdr/common/util/HashUtil.java
+++ src/main/java/uk/gov/ons/fsdr/common/util/HashUtil.java
@@ -18,9 +18,7 @@ public class HashUtil {
     try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ObjectOutputStream oos = new ObjectOutputStream(baos)) {
       oos.writeObject(object);
-      MessageDigest md = MessageDigest.getInstance("MD5");
-      md.update(baos.toByteArray());
-      byte[] thedigest = md.digest();
+      byte[] thedigest = MessageDigest.getInstance("MD5").digest(baos.toByteArray());
       return DatatypeConverter.printHexBinary(thedigest);
     } catch(NoSuchAlgorithmException | IOException e) {
       throw new FsdrException("Failed to create hash", e);
